bf94c8fd696e11e3ab06cd67cfb9e5e05b3a7e10
bismansahni <bismansahni@outlook.com>
2024-06-15 23:19:16 +0530

    updated the settings

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const https = require('https');
-const url = 'https://class-seat-checker.onrender.com/'; 
+
+const url = 'https://class-seat-checker.onrender.com/'; // Replace with your target URL
 
 function visitUrl() {
   https.get(url, (res) => {
-    console.log(`Status Code: ${res.statusCode}`);
+    let data = '';
+
     res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      console.log(`Status Code: ${res.statusCode}`);
+      console.log(`BODY: ${data}`);
     });
   }).on('error', (e) => {
     console.error(`Got error: ${e.message}`);
   });
 }
 
-// Call visitUrl every 2 minutes
-setInterval(visitUrl, 2 * 60 * 1000);
-
-// Initial call to start the interval
 visitUrl();
